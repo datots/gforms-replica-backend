9ac0ae247dd3a3ea93cecf9d26d11ecc9a6e2cd4
datots <dato_ts@hotmail.com>
2024-11-01 18:16:41 +0400

    update
    update

--- server.js
+++ server.js
@@ -1,39 +1,3 @@
-// //server.js
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import connectDB from "./config/db.js"; // Import database connection
-// import authRoutes from "./routes/authRoutes.js"; // Import combined authentication routes
-// import userRoutes from "./routes/user.js"; // Import user routes
-// import templateRoutes from "./routes/templateRoutes.js"; // Import template routes
-// import analyticsRouter from "./routes/analytics.js";
-// // Configure environment variables
-// dotenv.config();
-
-// const app = express();
-
-// // Connect to the database
-// connectDB();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Routes
-// app.use("/api/auth", authRoutes);
-// app.use("/api/user", userRoutes); // Add the user routes if needed
-// app.use("/api/template", templateRoutes); // Add the template routes
-// app.use("/api/analytics", analyticsRouter);
-// // Error handling middleware
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).json({ message: "An error occurred" });
-// });
-
-// // Server setup
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 // server.js
 import express from "express";
 import cors from "cors";
@@ -53,12 +17,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(
-  cors({
-    origin: "*", // Change this to your frontend URL
-    credentials: true, // Allow credentials if needed
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // Routes
